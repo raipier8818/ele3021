1127fa942f0416c713aa594bebfb310d5bb5b1f4
lake <raipier8818@hanyang.ac.kr>
2024-03-26 00:44:44 +0900

    update assignment1

---
+++ assignment1/project01.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+    char *student_id = "2020063045";
+    int pid = getpid();
+    int gpid = getgpid();
+    printf(1, "My student id is %s\n", student_id);
+    printf(1, "My pid is %d\n", pid);
+    printf(1, "My gpid is %d\n", gpid);
+
+    exit();
+}
