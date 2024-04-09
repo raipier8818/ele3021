8858afd3b3a35ca5e60f61e6a4666c77a057b169
lake <raipier8818@hanyang.ac.kr>
2024-04-09 21:02:17 +0900

    create project

---
+++ OS_project02_11741_2020063045/xv6-public/project01.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+    char *student_id = "2020063045";
+    int pid = getpid();
+    // int ppid = getppid();
+    int gpid = getgpid();
+    printf(1, "My student id is %s\n", student_id);
+    printf(1, "My pid is %d\n", pid);
+    // printf(1, "My ppid is %d\n", ppid);
+    printf(1, "My gpid is %d\n", gpid);
+
+    exit();
+}
